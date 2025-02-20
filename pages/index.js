1a1393b337fb8d3ab44bdd041deefda5f94adc78
salman-dev786 <salman.hafeez@sprintx.net>
2025-02-20 17:44:10 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,41 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
 export default function Home() {
-  return <h1>Hello, Next.js on AWS!</h1>;
+  const [text, setText] = useState("");
+  const [texts, setTexts] = useState([]);
+
+  useEffect(() => {
+    fetchTexts();
+  }, []);
+
+  async function fetchTexts() {
+    const res = await fetch("/api/text");
+    const data = await res.json();
+    setTexts(data);
+  }
+
+  async function addText() {
+    if (!text) return;
+    await fetch("/api/text", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ text }),
+    });
+    setText("");
+    fetchTexts();
+  }
+
+  return (
+    <div>
+      <h1>Simple To-Do App</h1>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={addText}>Add</button>
+
+      <ul>
+        {texts.map((item) => (
+          <li key={item._id}>{item.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
